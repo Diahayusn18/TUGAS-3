ac0436631da8eb928d95eaa28f392577107207d2
aminalkutbi <alkutbia4@gmail.com>
2022-01-15 16:13:03 +0800

    first commit

---
+++ app/src/main/java/simple/example/hewanpedia/model/Kupukupu.java
@@ -0,0 +1,9 @@
+package simple.example.hewanpedia.model;
+
+public class Kupukupu extends Hewan{
+
+    public Kupukupu(String ras, String asal, String deskripsi, int drawableRes) {
+        super("Kupukupu",ras,asal,deskripsi,drawableRes);
+
+    }
+}
